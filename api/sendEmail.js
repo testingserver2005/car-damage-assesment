ea8ddd98ef231414c4625954b85d86ad87fa64b8
testingserver2005 <testingserver2005@gmail.com>
2026-03-07 10:36:32 +0000

    new changes

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -177,7 +177,7 @@ app.post("/api/sendEmail", upload.any(), async (req, res) => {
 
     <p style="margin-top: 30px;">Med venlig hilsen,<br/>
     <strong>QuickRepair.dk</strong></p>
-        <img src="/logo.png" alt="QuickRepair.dk" style="height: 40px; margin-top: 8px; display: block;" /></p>
+        <img src="https://skadesanmeldelse.quickrepair.dk/logo.png" alt="QuickRepair.dk" style="height: 40px; margin-top: 8px; display: block;" /></p>
 
 
     <hr style="border: none; border-top: 1px solid #ddd; margin: 20px 0;" />
